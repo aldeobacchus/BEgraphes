0cb6b00080ee5dfb2c231f8e9e8bd1624fc54c1e
Bordais--Poulard Gabriel <bordais-@insa-20168>
2021-04-12 12:18:21 +0200

    Dijkstra  suite implementation

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -28,9 +28,14 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
         //On déclare le tas binaire qui va contenir les label parcouru
         BinaryHeap<Label> tas = new BinaryHeap<>();
         
+        //On declare le cout a l'origine a 0 et on l'insere dans le tas
         etiquettes.get(data.getOrigin()).setCost(0);
         tas.insert(etiquettes.get(data.getOrigin()));
         
+        //tant qu'il existe des sommets non marqués
+        while ( tas.isEmpty() == false) {
+        	
+        }
         
         return solution;
     }
